d4019294bd05c9ff6619e07d9e6a12c7420a9596
miraj75 <mirajhossen30@gmail.com>
2024-12-11 21:45:07 +0600

    first commit

---
+++ Biodata.java
@@ -0,0 +1,14 @@
+public class Biodata {
+    public static void main(String[] args){
+        String name ="Miraj";
+        String name2 ="Maria";
+        int a =10;
+        int b = 5;
+        int mul = a* b;
+        int min = a-b;
+        System.out.println(mul/min);
+        System.out.println(name + " Love " + name2);
+        System.out.print("*\n**\n***\n****\n*****\n****\n***\n**\n*");
+        
+    }
+}
---
+++ Product.java
@@ -0,0 +1,10 @@
+public class Product {
+    public static void main(String[] args){
+        int id = 101;
+        String titile = "iphone15";
+        int price = 1500;
+        String description = "perfect product with best image quality";
+        String category = "phone";
+        System.out.println("id: "+ id+ " title: "+ titile + " price: " + price + "description: " + description + " category " + category);
+    }
+}
